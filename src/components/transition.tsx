0476b55896ed9f30ddedac267e5798a488119044
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-26 01:43:11 +0300

    pointer update

--- src/components/transition.tsx
+++ src/components/transition.tsx
@@ -15,7 +15,7 @@ export default function Transition() {
   return (
     <main
       className={`bg-black/70 backdrop-blur-lg flex flex-col gap-12 w-screen h-screen justify-center items-center fixed top-0 left-0 z-30 duration-300 ${
-        isDone && "opacity-0"
+        isDone && "opacity-0 z-0 pointer-events-none"
       }`}
     >
       <l-hourglass
