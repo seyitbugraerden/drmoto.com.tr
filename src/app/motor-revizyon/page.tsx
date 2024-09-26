ae29fbd9da7d60f6b957013f46298852411ef89b
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-26 14:33:28 +0300

    meta updates

--- src/app/motor-revizyon/page.tsx
+++ src/app/motor-revizyon/page.tsx
@@ -6,55 +6,15 @@ const page = () => {
   return (
     <>
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <meta
           name="keywords"
           content="motosiklet servisi, motosiklet tamiri, motosiklet bakımı, Ankara motosiklet servisi"
-        />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:title" content="Dr. Moto - Motosiklet Servisi" />
-        <meta
-          property="og:description"
-          content="Motosiklet bakım, onarım ve tamir hizmetleri."
-        />
-        <meta
-          property="og:image"
-          content="https://www.drmoto.com.tr/favicon.ico"
-        />
-        <meta
-          name="twitter:image"
-          content="https://www.drmoto.com.tr/favicon.ico"
-        />
-        <meta property="og:url" content="https://www.drmoto.com.tr/" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Dr. Moto - Motosiklet Servisi" />
-        <meta
-          name="twitter:description"
-          content="Motosiklet bakım, onarım ve tamir hizmetleri."
         />
         <meta
           name="description"
           content="Dr. Moto, motor revizyon işlemlerinde uzman ekibiyle motosikletinizin performansını yeniden kazanmasına yardımcı olur. Güvenilir ve etkili çözümler için bize danışın!"
         />
         <title>Dr. Moto - Motor Revizyon İşlemleri | Ankara</title>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <>
         <div className="w-screen h-[30dvh] bg-black/90"></div>
@@ -65,15 +25,15 @@ const page = () => {
                 Motor Revizyon İşlemleri
               </h2>
               <p className="text-justify text-black/50">
-                <strong>Dr. Moto</strong>'da motorunuzun uzun ömürlü ve güvenli
-                bir şekilde çalışması için profesyonel yağ değişimi ve genel
-                bakım hizmetleri sunuyoruz. Düzenli yağ değişimi, motor
+                <strong>Dr. Moto</strong>&apos;da motorunuzun uzun ömürlü ve
+                güvenli bir şekilde çalışması için profesyonel yağ değişimi ve
+                genel bakım hizmetleri sunuyoruz. Düzenli yağ değişimi, motor
                 performansınızı en üst düzeyde tutarken aşınmayı en aza indirir.
                 Ayrıca, genel bakım kapsamında frenlerden zincirlere,
                 filtrelerden lastik basıncına kadar tüm kritik bileşenleri
                 kontrol ederek sorunsuz bir sürüş deneyimi sağlamayı
                 amaçlıyoruz. Motorunuzun sağlığını korumak ve her yolculukta
-                maksimum verim alabilmek için, Dr. Moto'da titizlikle
+                maksimum verim alabilmek için, Dr. Moto&apos;da titizlikle
                 gerçekleştirilen bu bakımları ihmal etmeyin.
               </p>
               <ul className="list-disc mt-2">
